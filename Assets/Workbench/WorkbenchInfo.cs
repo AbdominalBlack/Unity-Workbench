306ef7624c2c5c84c2e14286318dca982cb189e5
FuHei <578966688@qq.com>
2023-03-05 20:08:41 +0800

    初始化工作台项目

---
+++ Assets/Workbench/WorkbenchInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Workbench
+{
+    [CreateAssetMenu(fileName = "Workbench", menuName = "工作台")]
+    public class WorkbenchInfo : ScriptableObject
+    {
+        public string Function;
+        public string Date;
+        public string Author;
+        [Multiline(3)]public string Description;
+
+        public List<DefaultAsset> DirectoryList = new List<DefaultAsset>();
+    }
+}
---
+++ Assets/Workbench/WorkbenchWindow.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace Workbench
+{
+    public class WorkbenchWindow : EditorWindow
+    {
+        private static Dictionary<int, WorkbenchWindow> sCacheWindows = new Dictionary<int, WorkbenchWindow>();
+
+        private WorkbenchInfo mInfo;
+        
+        
+        
+        
+        [OnOpenAsset(0)]
+        public static bool Open(int instanceID, int line)
+        {
+            if (!(EditorUtility.InstanceIDToObject(instanceID) is WorkbenchInfo workbenchInfo)) return false;
+             
+            if (!sCacheWindows.TryGetValue(instanceID,out WorkbenchWindow window))
+            {
+                WorkbenchWindow[] windows = Resources.FindObjectsOfTypeAll<WorkbenchWindow>();
+                foreach (WorkbenchWindow item in windows)
+                {
+                    if (item.mInfo==workbenchInfo)
+                    {
+                        window = item;
+                        break;
+                    }
+                }
+
+                if (window==null)
+                {
+                    window = CreateWindow<WorkbenchWindow>();
+                }
+                sCacheWindows.Add(instanceID, window);
+            }
+
+            string title = string.IsNullOrEmpty(workbenchInfo.Function) ? "Workbench" : $"Workbench - {workbenchInfo.Function}";
+            window.titleContent = new GUIContent(title, EditorGUIUtility.IconContent("d_Favorite Icon").image);
+            window.mInfo = workbenchInfo;
+            window.minSize = new Vector2(600, 400);
+            window.Show();
+            window.Focus();
+            
+            return true;
+        }
+
+        private void OnDestroy()
+        {
+            sCacheWindows.Remove(mInfo.GetInstanceID());
+        }
+    }
+}
