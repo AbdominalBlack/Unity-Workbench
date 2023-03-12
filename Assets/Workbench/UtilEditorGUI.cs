dc43021c453ae47c7d0a1b0924e2f7c9f6a35d0b
FuHei <578966688@qq.com>
2023-03-12 18:37:14 +0800

    更新编辑器窗口逻辑

--- Assets/Workbench/UtilEditorGUI.cs
+++ Assets/Workbench/UtilEditorGUI.cs
@@ -1,7 +1,102 @@
-﻿namespace Workbench
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace Workbench
 {
-    public class UtilEditorGUI
+    public static class UtilEditorGUI
     {
+        private static int sMouseDeltaReaderHash = "MouseDeltaReader".GetHashCode();
+        private static Vector2 sMouseDeltaReaderLastPos;
+
+        private const BindingFlags FLAGS = BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public;
+        private static Func<Vector2,Vector2> sUnclip = Type.GetType("UnityEngine.GUIClip,UnityEngine").GetMethod("Unclip",FLAGS,null,new Type[]{typeof(Vector2)},null).CreateDelegate(typeof(Func<Vector2,Vector2>)) as Func<Vector2,Vector2>;
+        private static Func<Rect, Event, bool> sHitTest = typeof(GUIUtility).GetMethod("HitTest", FLAGS,null,new Type[]{typeof(Rect),typeof(Event)},null).CreateDelegate(typeof(Func<Rect, Event, bool>)) as Func<Rect, Event, bool>;
         
+            
+            
+        public static Rect HandleHorizontalSplitter(Rect dragRect, float width, float minLeftSide, float minRightSide)
+        {
+            bool flag = Event.current.type == EventType.Repaint;
+            if (flag)
+            {
+                EditorGUIUtility.AddCursorRect(dragRect, MouseCursor.SplitResizeLeftRight);
+            }
+            float num = 0f;
+            float x = MouseDeltaReader(dragRect, true).x;
+            bool flag2 = x != 0f;
+            if (flag2)
+            {
+                dragRect.x += x;
+                num = Mathf.Clamp(dragRect.x, minLeftSide, width - minRightSide);
+            }
+            bool flag3 = dragRect.x > width - minRightSide;
+            if (flag3)
+            {
+                num = width - minRightSide;
+            }
+            bool flag4 = num > 0f;
+            if (flag4)
+            {
+                dragRect.x = num;
+            }
+            return dragRect;
+        }
+        
+        public static Vector2 MouseDeltaReader(Rect position, bool activated)
+        {
+            int controlID = GUIUtility.GetControlID(sMouseDeltaReaderHash, FocusType.Passive, position);
+            Event current = Event.current;
+            switch (current.GetTypeForControl(controlID))
+            {
+                case EventType.MouseDown:
+                {
+                    bool flag = activated && GUIUtility.hotControl == 0 && sHitTest.Invoke(position, current) && current.button == 0;
+                    if (flag)
+                    {
+                        GUIUtility.hotControl = controlID;
+                        GUIUtility.keyboardControl = 0;
+                        sMouseDeltaReaderLastPos = sUnclip.Invoke(current.mousePosition);
+                        current.Use();
+                    }
+                    break;
+                }
+                case EventType.MouseUp:
+                {
+                    bool flag2 = GUIUtility.hotControl == controlID && current.button == 0;
+                    if (flag2)
+                    {
+                        GUIUtility.hotControl = 0;
+                        current.Use();
+                    }
+                    break;
+                }
+                case EventType.MouseDrag:
+                {
+                    bool flag3 = GUIUtility.hotControl == controlID;
+                    if (flag3)
+                    {
+                        Vector2 a = sUnclip.Invoke(current.mousePosition);
+                        Vector2 result = a - sMouseDeltaReaderLastPos;
+                        sMouseDeltaReaderLastPos = a;
+                        current.Use();
+                        return result;
+                    }
+                    break;
+                }
+            }
+            return Vector2.zero;
+        }
+        
+        public static void DrawHorizontalSplitter(Rect dragRect)
+        {
+            if (Event.current.type != UnityEngine.EventType.Repaint)
+                return;
+            Color color = GUI.color;
+            GUI.color *= EditorGUIUtility.isProSkin ? new Color(0.12f, 0.12f, 0.12f, 1.333f) : new Color(0.6f, 0.6f, 0.6f, 1.333f);
+            GUI.DrawTexture(new Rect(dragRect.x - 1f, dragRect.y, 1f, dragRect.height), (Texture) EditorGUIUtility.whiteTexture);
+            GUI.color = color;
+        }
     }
 }
--- Assets/Workbench/WorkbenchWindow.cs
+++ Assets/Workbench/WorkbenchWindow.cs
@@ -10,11 +10,9 @@ namespace Workbench
     {
         private static Dictionary<int, WorkbenchWindow> sCacheWindows = new Dictionary<int, WorkbenchWindow>();
 
+        private int mInstance;
         private WorkbenchInfo mInfo;
-        
-        
-        
-        
+
         [OnOpenAsset(0)]
         public static bool Open(int instanceID, int line)
         {
@@ -25,11 +23,9 @@ namespace Workbench
                 WorkbenchWindow[] windows = Resources.FindObjectsOfTypeAll<WorkbenchWindow>();
                 foreach (WorkbenchWindow item in windows)
                 {
-                    if (item.mInfo==workbenchInfo)
-                    {
-                        window = item;
-                        break;
-                    }
+                    if (item.mInfo != workbenchInfo) continue;
+                    window = item;
+                    break;
                 }
 
                 if (window==null)
@@ -42,6 +38,7 @@ namespace Workbench
             string title = string.IsNullOrEmpty(workbenchInfo.Function) ? "Workbench" : $"Workbench - {workbenchInfo.Function}";
             window.titleContent = new GUIContent(title, EditorGUIUtility.IconContent("d_Favorite Icon").image);
             window.mInfo = workbenchInfo;
+            window.mInstance = instanceID;
             window.minSize = new Vector2(600, 400);
             window.Show();
             window.Focus();
@@ -51,7 +48,57 @@ namespace Workbench
 
         private void OnDestroy()
         {
-            sCacheWindows.Remove(mInfo.GetInstanceID());
+            sCacheWindows.Remove(mInstance);
         }
+
+        private float mToolbarHeight = 21;
+        private float mLastListWidth;
+        [SerializeField]
+        private float mDirectoriesAreaWidth = 115f;
+        private float mMinDirectoriesAreaWidth = 110f;
+        [NonSerialized]
+        private Rect mListAreaRect;
+        [NonSerialized]
+        private Rect mTreeViewRect;
+
+        private void OnGUI()
+        {
+            if (mInfo==null)
+            {
+                Close();
+            }
+            this.ResizeHandling(base.position.width, base.position.height - mToolbarHeight);
+            // if (this.m_SearchFilter.IsSearching())
+            //     this.SearchAreaBar();
+            // else
+            //     this.BreadCrumbBar();
+            //this.m_FolderTree.OnGUI(this.m_TreeViewRect, this.m_TreeViewKeyboardControlID);
+            //this.m_ListArea.OnGUI(this.m_ListAreaRect, this.m_ListKeyboardControlID);
+            UtilEditorGUI.DrawHorizontalSplitter(new Rect(this.mListAreaRect.x + 1f,  mToolbarHeight, 1f, base.position.height - mToolbarHeight));
+            // if (this.m_SearchFilter.GetState() == SearchFilter.State.FolderBrowsing && this.m_ListArea.numItemsDisplayed == 0)
+            // {
+            //     Vector2 vector2 = EditorStyles.label.CalcSize(ProjectBrowser.s_Styles.m_EmptyFolderText);
+            //     Rect position3 = new Rect(this.m_ListAreaRect.x + 2f + Mathf.Max(0.0f, (float) (((double) this.m_ListAreaRect.width - (double) vector2.x) * 0.5)), this.m_ListAreaRect.y + 10f, vector2.x, 20f);
+            //     using (new EditorGUI.DisabledScope(true))
+            //         GUI.Label(position3, ProjectBrowser.s_Styles.m_EmptyFolderText, EditorStyles.label);
+            // }
+          
+        }
+
+
+        private void ResizeHandling(float width, float height)
+        {
+            Rect dragRect = new Rect(this.mDirectoriesAreaWidth, mToolbarHeight, 5f, height);
+            dragRect = UtilEditorGUI.HandleHorizontalSplitter(dragRect, base.position.width, this.mMinDirectoriesAreaWidth, 230f - this.mMinDirectoriesAreaWidth);
+            this.mDirectoriesAreaWidth = dragRect.x;
+            float num = base.position.width - this.mDirectoriesAreaWidth;
+            bool flag2 = num != this.mLastListWidth;
+            if (flag2)
+            {
+                //this.RefreshSplittedSelectedPath();
+            }
+            this.mLastListWidth = num;
+        }
+
     }
 }
